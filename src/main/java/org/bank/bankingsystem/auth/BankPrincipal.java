eb68705464412e1bfca734995605210bba0e7050
milanarif <milanarif@gmail.com>
2022-04-26 16:08:14 +0200

    fix broken code

--- src/main/java/org/bank/bankingsystem/auth/BankPrincipal.java
+++ src/main/java/org/bank/bankingsystem/auth/BankPrincipal.java
@@ -1,26 +1,14 @@
 package org.bank.bankingsystem.auth;
 
 import java.util.Collection;
-import java.util.Set;
 
-import org.bank.bankingsystem.entity.AccountEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class BankPrincipal implements UserDetails {
 
-    private final AccountEntity accountEntity;
-
-    public BankPrincipal(AccountEntity accountEntity) {
-        this.accountEntity = accountEntity;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<
-
-
-
         // TODO Auto-generated method stub
         return null;
     }
@@ -60,5 +48,7 @@ public class BankPrincipal implements UserDetails {
         // TODO Auto-generated method stub
         return false;
     }
+
+  
     
 }
