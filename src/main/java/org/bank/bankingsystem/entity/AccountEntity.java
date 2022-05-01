993b890ecd55a69a2ff9ec371ab1160801413819
gustav442 <gustavjak94@gmail.com>
2022-05-01 16:31:58 +0200

    UserEntity OneToOne

--- src/main/java/org/bank/bankingsystem/entity/AccountEntity.java
+++ src/main/java/org/bank/bankingsystem/entity/AccountEntity.java
@@ -1,10 +1,6 @@
 package org.bank.bankingsystem.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 public class AccountEntity {
@@ -15,7 +11,7 @@ public class AccountEntity {
 
     Long funds;
 
-    @ManyToOne
+    @OneToOne
     private UserEntity user;
 
     @ManyToOne
