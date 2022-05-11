33ef2855d0bf213057a5e2cbd21c14614bbe66e0
PL <philip.lundin@iths.se>
2022-05-11 14:21:11 +0200

    add security if ROLE_USER make requests

--- src/main/java/org/bank/bankingsystem/security/JwtRequestFilter.java
+++ src/main/java/org/bank/bankingsystem/security/JwtRequestFilter.java
@@ -1,5 +1,8 @@
 package org.bank.bankingsystem.security;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,6 +16,7 @@ import org.bank.bankingsystem.repository.UserRepository;
 import org.bank.bankingsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -57,9 +61,30 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		} else {
 			logger.warn("JWT Token does not begin with Bearer String");
 		}
-		// check if token belongs to user's account
-		if (request.getRequestURI().startsWith("/accounts/transaction/")) {
-			checkTransactionAccountOwner(jwtToken, request);
+
+		if (username != null) {
+			UserDetails getUserRole = this.jwtUserDetailsService.loadUserByUsername(username);
+			Collection<? extends GrantedAuthority> authorities = getUserRole.getAuthorities();
+			String userRole = authorities.toString().substring(1, authorities.toString().length() - 1);
+
+			if (userRole.equals("ROLE_USER")) {
+				String requestUri = request.getRequestURI();
+				// check if token belongs to user
+				if (!requestUri.equals("/users/signup") &&
+						!requestUri.equals("/users/findAllLoans") &&
+						requestUri.startsWith("/users/")) {
+					checkUserTokenOwner(jwtToken, request);
+				}
+				// check if token belongs to user's account
+				if (requestUri.startsWith("/accounts/transaction/") ||
+						requestUri.startsWith("/accounts/deposit/") ||
+						requestUri.startsWith("/accounts/withdraw/")) {
+					checkTransactionAccountOwner(jwtToken, request);
+				} else if (!requestUri.equals("/accounts/transactions") &&
+						requestUri.startsWith("/accounts/")) {
+					checkUserAccountOwner(jwtToken, request);
+				}
+			}
 		}
 		//Once we get the token validate it.
 		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -80,6 +105,35 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		chain.doFilter(request, response);
 	}
 
+	protected void checkUserTokenOwner(String jwtToken, HttpServletRequest request) {
+		String requestUri = request.getRequestURI();
+		String userIdStr = requestUri.split("/")[2];
+		if (userIdStr.length() < 6) {
+			Long userId = Long.valueOf(userIdStr);
+			String tokenUsername = jwtTokenUtil.getUsernameFromToken(jwtToken);
+			UserEntity findUserByUsername = userRepository.findByUsername(tokenUsername);
+			Long findUserId = findUserByUsername.getId();
+
+			if (userId != findUserId) {
+				throw new CustomException.InvalidUserDetails("Authorization does not match");
+			}
+		}
+	}
+
+	protected void checkUserAccountOwner(String jwtToken, HttpServletRequest request) {
+		String requestUri = request.getRequestURI();
+		String userIdStr = requestUri.split("/")[2];
+		Long userId = Long.valueOf(userIdStr);
+
+		String tokenUsername = jwtTokenUtil.getUsernameFromToken(jwtToken);
+		UserEntity findUserByUsername = userRepository.findByUsername(tokenUsername);
+		Long findUserId = findUserByUsername.getAccount().getAccountNumber();
+
+		if (userId != findUserId) {
+			throw new CustomException.UnauthorizedTransfer("Authorization does not match");
+		}
+	}
+
 	protected void checkTransactionAccountOwner(String jwtToken, HttpServletRequest request) {
 		String requestURI = request.getRequestURI();
 		String senderAccountStr = requestURI.split("/")[3];
@@ -90,8 +144,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		AccountEntity userAccount = findUserByUsername.getAccount();
 
 		if (userAccount.getAccountNumber() != senderAccount) {
-			throw new CustomException.UnauthorizedTransfer("You have no authorization for this account");
+			throw new CustomException.UnauthorizedTransfer("Authorization does not match");
 		}
 	}
-
 }
